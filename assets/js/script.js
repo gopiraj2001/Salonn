07c6af4c3f3e7b2caeeecb00534c53fdaa4d598f
Gopiraj Mane <137722204+gopiraj2001@users.noreply.github.com>
2025-08-13 16:52:56 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+'use strict';
+
+fetch('haircut.html')
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('haircut-section-placeholder').innerHTML = html;
+    });
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = () => navbar.classList.remove("active");
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+
+
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+
+addEventOnElem(filterBtns, "click", filter);
+
+/** Haircut */
+ const modal = document.createElement('div');
+  modal.classList.add('image-modal');
+  document.body.appendChild(modal);
+
+  // Open image in modal
+  document.querySelectorAll('.open-image').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const imgSrc = this.getAttribute('href');
+      modal.innerHTML = `<img src="${imgSrc}" alt=""><span class="close-btn"></span>`;
+      modal.classList.add('active');
+    });
+  });
+
+  // Close modal when clicked
+  modal.addEventListener('click', function () {
+    modal.classList.remove('active');
+  });
+
+  
